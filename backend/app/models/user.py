5176bf7f6cad36e0401d3683fbf67536f7f78164
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-10 15:42:48 +0100

    add User ORM model

---
+++ backend/app/models/user.py
@@ -0,0 +1,30 @@
+from .base import Base
+
+from sqlalchemy import String, text
+from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from .account import Account
+
+
+class User(Base):
+
+    # NOTE: total balance won't be stored here as there could be a problem 
+    # with a transactions synchronization, in order to get total balance 
+    # of all the accounts it will be calculated and returned to the client
+
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    full_name: Mapped[str] = mapped_column(String, nullable=False)
+    password: Mapped[str] = mapped_column(String, nullable=False)
+    logo_path: Mapped[str] = mapped_column(
+        String, nullable=False, server_default=text("/logos/default.png")
+    )
+    accounts: Mapped[list["Account"]] = mapped_column("Account", back_populates="owner")
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__} (id={self.id}, full_name="{self.full_name}")'
+
+    def __str__(self) -> str:
+        return f'{self.__class__.__name__} (id={self.id}, full_name="{self.full_name}")'
