12a6101047c809412a3c502af019198f138331a8
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-10 15:54:09 +0100

    code refactor

--- backend/app/models/mixins.py
+++ backend/app/models/mixins.py
@@ -14,7 +14,6 @@ class UserRelationMixin:
     _user_id_nullable: bool = False
     _user_back_populates: str | None = None
 
-
     @declared_attr
     def owner_id(cls) -> Mapped[int]:
         return mapped_column(
@@ -22,18 +21,17 @@ class UserRelationMixin:
             unique=cls._user_id_unique,
             nullable=cls._user_id_nullable,
         )
-    
+
     @declared_attr
     def owner(cls) -> Mapped["User"]:
         return relationship("User", back_populates=cls._user_back_populates)
-        
+
 
 class AccountRelationMixin:
-        
+
     _account_id_unique: bool = False
     _account_id_nullable: bool = False
     _account_back_populates: str | None = None
-
 
     @declared_attr
     def owner_id(cls) -> Mapped[int]:
@@ -42,7 +40,7 @@ class AccountRelationMixin:
             unique=cls._account_id_unique,
             nullable=cls._account_id_nullable,
         )
-    
+
     @declared_attr
     def owner(cls) -> Mapped["Account"]:
         return relationship("Account", back_populates=cls._account_back_populates)
