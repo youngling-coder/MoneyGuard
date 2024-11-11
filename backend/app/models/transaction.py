2f56242a8e57ec48ac8ef458761ee74b6cce1da4
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-11 15:35:32 +0100

    set description field as unnecessary

--- backend/app/models/transaction.py
+++ backend/app/models/transaction.py
@@ -11,9 +11,9 @@ class Trasaction(AccountRelationMixin, Base):
     _account_back_populates = "transactions"
 
     amount: Mapped[int] = mapped_column(BigInteger, nullable=False)
-    description: Mapped[str] = mapped_column(String)
+    description: Mapped[str] = mapped_column(String, nullable=True)
     type: Mapped[str] = mapped_column(Enum(TransactionType), nullable=False)
-    recipient: Mapped[str] = mapped_column(String)
+    recipient: Mapped[str] = mapped_column(String, nullable=False)
 
     def __repr__(self) -> str:
         return f'{self.__class__.__name__} (id={self.id}, type="{self.type}", amount="{self.amount}", recipient="{self.recipient}")'
