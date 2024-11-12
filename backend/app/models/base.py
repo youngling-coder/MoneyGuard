c46f32f1f63ada83c4be1423f30bf78f59ffcb5c
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-12 18:44:39 +0100

    edit timestamp field to get rid off timezone

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -1,4 +1,4 @@
-from datetime import datetime, UTC
+from datetime import datetime, timezone
 from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
 from sqlalchemy import func
 
@@ -10,9 +10,9 @@ class Base(DeclarativeBase):
     def __tablename__(cls) -> str:
         return f"{cls.__name__.lower()}s"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
     timestamp: Mapped[datetime] = mapped_column(
-        default=lambda: datetime.now(UTC), server_default=func.now()
+        default=lambda: datetime.now(timezone.utc).replace(tzinfo=None), server_default=func.now(), nullable=False
     )
 
 
