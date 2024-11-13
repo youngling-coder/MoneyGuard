9ffb03145f09cd661cdf7e14f991197a9f13520b
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-13 17:52:07 +0100

    add an endpoint that handles user deletion

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -1,7 +1,9 @@
+from typing import Optional
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from sqlalchemy import update
+from sqlalchemy import update, delete
 
 from ..database import get_db
 from .. import models, schemas, utils
@@ -17,8 +19,6 @@ async def signup(user: schemas.CreateUser, db: AsyncSession = Depends(get_db)):
     stmt = select(models.User).filter(models.User.email == user.email)
     result = await db.execute(stmt)
     user_exists = result.scalars().first()
-
-    print(user)
 
     if user_exists:
         raise HTTPException(
@@ -36,11 +36,8 @@ async def signup(user: schemas.CreateUser, db: AsyncSession = Depends(get_db)):
     await db.commit()
 
 
-from typing import Optional
-
-
 @router.put("/update", status_code=status.HTTP_200_OK, response_model=schemas.UserResponse)
-async def update_user(updated_user: schemas.UpdateUser, db: AsyncSession = Depends(get_db), current_user: Optional[models.User] = Depends(oauth2.get_current_user)):
+async def update_user(updated_user: schemas.UpdateUser, db: AsyncSession = Depends(get_db), current_user = Depends(oauth2.get_current_user)):
     
     target_user_stmt = select(models.User).filter(models.User.id == current_user.id)
     target_user_result = await db.execute(target_user_stmt)
@@ -56,3 +53,22 @@ async def update_user(updated_user: schemas.UpdateUser, db: AsyncSession = Depen
     await db.commit()
 
     return updated_user
+
+
+@router.get("/get", response_model=schemas.UserResponse)
+async def get_user(db: AsyncSession = Depends(get_db), current_user = Depends(oauth2.get_current_user)):
+    
+    stmt = select(models.User).filter(models.User.id == current_user.id)
+    result = await db.execute(stmt)
+    requested_user = result.scalars().first()
+
+    return requested_user
+
+
+@router.delete("/delete", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(db: AsyncSession = Depends(get_db), current_user = Depends(oauth2.get_current_user)):
+
+    stmt = delete(models.User).where(models.User.id == current_user.id)
+    await db.execute(stmt)
+    await db.commit()
+    
