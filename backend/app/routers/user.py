487c7a9b800824d696d8a1d8ffe9ff62629efcf4
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-13 17:02:39 +0100

    add endpoint that updates user profile information

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from sqlalchemy import update
+
 from ..database import get_db
 from .. import models, schemas, utils
+from ..oauth2 import oauth2
 
 
-router = APIRouter(prefix="/user", tags=["Users"])
+router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.post("/signup", status_code=status.HTTP_201_CREATED)
@@ -31,3 +34,25 @@ async def signup(user: schemas.CreateUser, db: AsyncSession = Depends(get_db)):
 
     db.add(new_user)
     await db.commit()
+
+
+from typing import Optional
+
+
+@router.put("/update", status_code=status.HTTP_200_OK, response_model=schemas.UserResponse)
+async def update_user(updated_user: schemas.UpdateUser, db: AsyncSession = Depends(get_db), current_user: Optional[models.User] = Depends(oauth2.get_current_user)):
+    
+    target_user_stmt = select(models.User).filter(models.User.id == current_user.id)
+    target_user_result = await db.execute(target_user_stmt)
+    target_user = target_user_result.scalars().first()
+
+    stmt = (
+        update(models.User).where(models.User.id == target_user.id).values(updated_user.model_dump()).execution_options(synchronize_session="fetch").returning(models.User)
+    )
+
+    updated_user_stmt = await db.execute(stmt)
+    updated_user = updated_user_stmt.scalars().first()
+    
+    await db.commit()
+
+    return updated_user
