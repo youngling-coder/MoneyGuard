16087432cb3a898288ec4358d6d669337c6cae27
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-15 15:22:55 +0100

    add utility to build profile_picture url

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -1,9 +1,9 @@
 __all__ = (
     "get_password_hash",
     "verify_password",
-    "get_profile_picture_if_exists"
+    "get_profile_picture_url"
 )
 
 from .password import get_password_hash
 from .password import verify_password
-from .profile_picture import get_profile_picture_if_exists
+from .profile_picture import get_profile_picture_url
--- backend/app/utils/profile_picture.py
+++ backend/app/utils/profile_picture.py
@@ -1,17 +1,16 @@
 import os
-from typing import Optional
 
 from ..settings import application_settings
 
 
-def get_profile_picture_if_exists(id: int) -> Optional[str]:
+def get_profile_picture_url(id: int) -> str:
     profile_picture_filename = f"{application_settings.profile_picture_base_name}_{id}{application_settings.profile_picture_extension}"
 
     print(profile_picture_filename)
 
-    profile_picture_path = os.path.join(application_settings.profile_picture_path, profile_picture_filename)
+    profile_picture_url = os.path.join(application_settings.profile_picture_path, profile_picture_filename)
 
-    print(profile_picture_path)
+    print(profile_picture_url)
 
-    if os.path.exists(profile_picture_path):
-        return profile_picture_path
+    return profile_picture_url
+    
