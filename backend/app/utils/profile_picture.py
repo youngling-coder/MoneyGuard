86d20ddf5148b2890a8f4b14e96076e541f095aa
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-16 13:31:23 +0100

    code refactor

--- backend/app/utils/profile_picture.py
+++ backend/app/utils/profile_picture.py
@@ -4,7 +4,7 @@ from ..settings import application_settings
 
 
 def get_profile_picture_url(id: int) -> str:
-    profile_picture_filename = f"{application_settings.profile_picture_base_name}_{id}{application_settings.profile_picture_extension}"
+    profile_picture_filename = f"{application_settings.profile_picture_filename}{id}{application_settings.profile_picture_extension}"
 
     print(profile_picture_filename)
 
