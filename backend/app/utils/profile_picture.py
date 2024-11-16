6112aabe117989c6343b13a4e60cb4d453ad18f5
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-16 15:35:28 +0100

    edit profile picture filename

--- backend/app/utils/profile_picture.py
+++ backend/app/utils/profile_picture.py
@@ -4,7 +4,7 @@ from ..settings import application_settings
 
 
 def get_profile_picture_url(id: int) -> str:
-    profile_picture_filename = f".{application_settings.profile_picture_path}/{application_settings.profile_picture_filename}{id}{application_settings.profile_picture_extension}"
+    profile_picture_filename = f"{application_settings.profile_picture_path}/{application_settings.profile_picture_filename}{id}{application_settings.profile_picture_extension}"
 
     profile_picture_url = os.path.join(
         application_settings.profile_picture_path, profile_picture_filename
