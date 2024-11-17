34a13df31a8afd22c28725b5dcde6ae10ef94cd9
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-17 16:09:23 +0100

    code refactor

--- backend/app/smtp/templates.py
+++ backend/app/smtp/templates.py
@@ -7,7 +7,7 @@ Next steps to follow are really simple:
 
 1. Login into your account
 2. Confirm your email using link below
-    http://localhost/8000/email_confirmation_url
+    http://localhost/8000email_confirmation_url
 3. Start using our application!
 
 Sincerely, 
