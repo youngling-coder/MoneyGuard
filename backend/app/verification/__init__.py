cd4a814a4d801461fb98ed4acdccbd1ddfb378d0
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-17 16:37:00 +0100

    add functions to __init__.py for exporting

---
+++ backend/app/verification/__init__.py
@@ -0,0 +1,7 @@
+__all__ = (
+    "verify_confirmation_token",
+    "generate_confirmation_token"
+)
+
+from .verification import verify_confirmation_token
+from .verification import generate_confirmation_token
