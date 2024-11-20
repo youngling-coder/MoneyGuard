e8363cb61a95d9444cd4cc897b084dac35ae47bb
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-20 18:57:13 +0100

    code refactor

--- backend/app/schemas/account.py
+++ backend/app/schemas/account.py
@@ -4,21 +4,21 @@ from datetime import datetime
 
 class Account(BaseModel):
     title: str
+    primary_account_number: str
+    card_verification_value: str
+    expiration_date: datetime
 
 
 class CreateAccount(Account):
-    primary_card_number: str
+    pass
 
 
 class AccountResponse(Account):
     balance: int
-    primary_account_number: str
-    card_verification_number: str
-    expiration_date: datetime
     timestamp: datetime
 
     model_config = ConfigDict(from_attributes=True)
 
 
-class UpdateAccount(CreateAccount):
+class UpdateAccount(Account):
     balance: int
