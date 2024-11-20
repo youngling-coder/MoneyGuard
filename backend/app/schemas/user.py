56cc50499325e0adbd53709d46c14702e38009ff
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-20 18:58:13 +0100

    add accounts field to UserResponse Pydantic schema to return all the accounts of the user

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,15 +1,13 @@
 from pydantic import BaseModel, ConfigDict, EmailStr
+from .account import AccountResponse
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 
 
 class User(BaseModel):
     name: str
     surname: str
-    profession: Optional[str]
-    country: Optional[str]
-    city: Optional[str]
-    birthdate: Optional[datetime]
+
 
 
 class CreateUser(User):
@@ -17,16 +15,20 @@ class CreateUser(User):
     password: str
 
 
-class UserResponse(User):
-    id: int
-    email: str
-    profile_picture: Optional[str]
-    timestamp: datetime
-    model_config = ConfigDict(from_attributes=True)
-
-
 class UpdateUser(User):
     email: EmailStr
+    profession: Optional[str]
+    country: Optional[str]
+    city: Optional[str]
+    birthdate: Optional[datetime]
+
+class UserResponse(UpdateUser):
+    id: int
+    profile_picture: Optional[str]
+    timestamp: datetime
+    accounts: List[AccountResponse]
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class UpdatePassword(BaseModel):
