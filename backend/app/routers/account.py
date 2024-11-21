ce4b72c83197841def0575aeb82e49edbc69b66e
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-21 16:49:09 +0100

    add an endpoint to process account deletion

--- backend/app/routers/account.py
+++ backend/app/routers/account.py
@@ -1,6 +1,9 @@
+from typing import Annotated
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy import delete
+from fastapi import APIRouter, Depends, HTTPException, status, Path
 
 from .. import models, schemas
 from ..database import get_db
@@ -36,3 +39,22 @@ async def get_accounts(db: AsyncSession = Depends(get_db), current_user: models.
     accounts = result.scalars().all()
 
     return accounts
+
+
+@router.delete("/delete/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_account(id: Annotated[int, Path()], db: AsyncSession = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
+
+    stmt = select(models.Account).filter(models.Account.id == id, models.Account.owner_id == current_user.id)
+    result = await db.execute(stmt)
+    account_exists = result.scalars().first()
+
+    if not account_exists:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No matching account found!"
+        )
+    
+    
+    stmt = delete(models.Account).where(models.Account.id == id)
+    await db.execute(stmt)
+    await db.commit()
