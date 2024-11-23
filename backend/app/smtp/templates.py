3bc54f37185762a2f3b845cc14c0006c09518479
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-23 23:43:16 +0100

    create a variable for email signature

--- backend/app/smtp/templates.py
+++ backend/app/smtp/templates.py
@@ -1,17 +1,21 @@
 class EmailTemplates:
+    SIGNATURE = """
+Sincerely, 
+Your MoneyGuard Team
+"""
+
     SIGNUP_EMAIL_VERIFICATION_TEMPLATE = f"""\
 Hello and welcome to MoneyGuard!
 
 We are glad to inform you that you've successfully signed up!
-Next steps to follow are really simple:
+Next steps to follow are really simple:\
 
 1. Login into your account
 2. Confirm your email using link below
     http://localhost:8000email_confirmation_url
 3. Start using our application!
 
-Sincerely, 
-Your MoneyGuard Team
+{SIGNATURE}
 """
 
     EMAIL_VERIFICATION_TEMPLATE = f"""\
@@ -21,6 +25,6 @@ To confirm new email, click your email verification link below
 
     http://localhost:8000email_confirmation_url
 
-Sincerely, 
-Your MoneyGuard Team
+
+{SIGNATURE}
 """
