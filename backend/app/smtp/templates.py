c2e8484324a6f528474b005cb26e951848f53ef9
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-23 23:51:00 +0100

    edit templates to improve clarity, formal tone and structured instructions

--- backend/app/smtp/templates.py
+++ backend/app/smtp/templates.py
@@ -5,26 +5,31 @@ Your MoneyGuard Team
 """
 
     SIGNUP_EMAIL_VERIFICATION_TEMPLATE = f"""\
-Hello and welcome to MoneyGuard!
+Welcome to MoneyGuard! We are thrilled to have you on board.  
 
-We are glad to inform you that you've successfully signed up!
-Next steps to follow are really simple:\
+To get started, please follow these simple steps:
 
-1. Login into your account
-2. Confirm your email using link below
+1. Log in to your account.  
+2. Confirm your email by clicking the link below:  
     http://localhost:8000email_confirmation_url
-3. Start using our application!
+3. Begin using MoneyGuard to manage and safeguard your finances!  
 
 {SIGNATURE}
 """
 
     EMAIL_VERIFICATION_TEMPLATE = f"""\
-The email has been updated successfully!
+We have updated your email address as requested.
 
-To confirm new email, click your email verification link below
-
+To complete this process, please confirm your new email by clicking the link below:
     http://localhost:8000email_confirmation_url
 
+If you did not request this update, please follow these steps to secure your account:  
+
+1. Log in to your account immediately and change your email
+2. Reset your password to protect your account
+3. Contact our support team if you suspect unauthorized activity or have any problems accessing your account.
+
+If you do not have a MoneyGuard account, please ignore this email.  
 
 {SIGNATURE}
 """
