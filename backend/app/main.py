5be73046ee369aa3725d5ee56a9306c91e22156f
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-11-23 23:51:43 +0100

    add basic CORS policies

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 from .routers import auth, user, account
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = [
+   "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(auth.router)
 app.include_router(user.router)
