73b40c99d3c26146b7fd60d00d31cdeea2eaeaae
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-12-17 16:34:17 +0100

    add generate_security_code function for exporting

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ("get_hash", "verify_hash", "get_profile_picture_url")
+__all__ = ("get_hash", "verify_hash", "get_profile_picture_url", "generate_security_code")
 
 from .hash import get_hash
 from .hash import verify_hash
+from .security_code import generate_security_code
 from .profile_picture import get_profile_picture_url
