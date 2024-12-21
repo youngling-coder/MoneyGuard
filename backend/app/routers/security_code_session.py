60f95ce339ade82b4eb0c35baae3e2cbf5768a49
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-12-21 16:39:38 +0100

    create an endpoint to handle security code session actions

---
+++ backend/app/routers/security_code_session.py
@@ -0,0 +1,16 @@
+from typing import Annotated
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy import delete, update
+from fastapi import APIRouter, Body, Depends, HTTPException, Query, status, Path
+
+from .. import models, schemas
+from ..database import get_db
+from ..oauth2 import oauth2
+
+
+router = APIRouter(
+    prefix="/security_code_session",
+    tags=["Security Code Session"]
+)
