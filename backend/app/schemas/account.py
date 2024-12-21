624dd4bb600d07230dde7f26b7430aec8861b09d
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-12-21 15:11:25 +0100

    improve Account response structure by splitting into base and transactions response components

--- backend/app/schemas/account.py
+++ backend/app/schemas/account.py
@@ -1,5 +1,8 @@
+from typing import List
+
 from pydantic import BaseModel, ConfigDict
 from datetime import datetime
+from .transaction import TransactionResponse
 
 
 class Account(BaseModel):
@@ -13,13 +16,14 @@ class CreateAccount(Account):
     pass
 
 
-class AccountResponse(Account):
+class AccountBaseResponse(Account):
     id: int
     balance: int
     timestamp: datetime
 
+class AccountTransactionsResponse(AccountBaseResponse):
+    transactions: List[TransactionResponse]
     model_config = ConfigDict(from_attributes=True)
-
 
 class UpdateAccount(Account):
     pass
