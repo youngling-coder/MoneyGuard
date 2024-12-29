03d93d325d0bae5fbd2a0f0caf465ecbfa23037b
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-12-29 14:53:06 +0100

    mount static files to the application to provide access to profile pictures

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI
-from .routers import auth, user, account, security_code_session
+import os
+
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, HTTPException, status
+from fastapi.staticfiles import StaticFiles
+
+from .routers import auth, user, account, security_code_session
+from .settings import application_settings
 
 app = FastAPI()
 
@@ -14,6 +19,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+if not os.path.exists(application_settings.profile_pictures_path):
+    try:
+        os.mkdir(application_settings.profile_pictures_path)
+    except FileExistsError:
+        pass
+    except PermissionError:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error: Permission Denied")
+    except OSError as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error: Unable to create directory")
+
+app.mount(application_settings.profile_pictures_mount_point, StaticFiles(directory=application_settings.profile_pictures_path), name="static")
 
 app.include_router(auth.router)
 app.include_router(user.router)
