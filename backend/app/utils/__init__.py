8e906e8e883e44ed0f05c34f167c466ff67463ec
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-12-29 14:17:00 +0100

    separate get profile picture path and url functionality

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -2,9 +2,10 @@ __all__ = (
     "get_hash",
     "verify_hash",
     "generate_security_code",
-    "get_profile_picture_url",
+    "get_profile_picture_path",
 )
 from .hash import get_hash
 from .hash import verify_hash
+from .profile_picture import get_profile_picture_path
 from .profile_picture import get_profile_picture_url
 from .security_code import generate_security_code
--- backend/app/utils/profile_picture.py
+++ backend/app/utils/profile_picture.py
@@ -1,14 +1,25 @@
 import os
+from  typing import Annotated
 
 from ..settings import application_settings
 
 
-def get_profile_picture_url(id: int) -> str:
-    profile_picture_filename = f"{application_settings.profile_picture_path}/{application_settings.profile_picture_filename}{id}{application_settings.profile_picture_extension}"
+def __profile_picture_filename(id: int) -> str:
+    return f"{application_settings.profile_picture_filename}{id}{application_settings.profile_picture_extension}"
 
-    profile_picture_url = os.path.join(
-        application_settings.profile_picture_path, profile_picture_filename
+def get_profile_picture_path(id: int) -> str:
+    profile_picture_filename = f"{application_settings.profile_pictures_path}/{__profile_picture_filename(id=id)}"
+
+    profile_picture_path = os.path.join(
+        application_settings.profile_pictures_path, profile_picture_filename
     )
-    profile_picture_url = os.path.abspath(profile_picture_filename)
+    profile_picture_path = os.path.abspath(profile_picture_filename)
+
+    return profile_picture_path
+
+
+def get_profile_picture_url(id: int, host: str = "https://cool-kite-divine.ngrok-free.app") -> str:
+    
+    profile_picture_url = f"{host}{application_settings.profile_pictures_mount_point}/{__profile_picture_filename(id=id)}"
 
     return profile_picture_url
