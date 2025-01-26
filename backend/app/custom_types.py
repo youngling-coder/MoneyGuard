e240f3723ca852ccb732ba1db7e40b09a97e95b4
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2025-01-26 15:59:00 +0100

    create alembic revision to rename description field in the database

--- backend/app/custom_types.py
+++ backend/app/custom_types.py
@@ -6,6 +6,19 @@ class TransactionType(Enum):
     expense = "expense"
 
 
+class TransactionCategory(Enum):
+    income = "Income"
+    savings_investments = "Savings & Investments"
+    bills_utilities = "Bills & Utilities"
+    rent_mortgage = "Rent & Mortgage"
+    groceries_dining = "Groceries & Dining"
+    transportation = "Transportation"
+    shopping_personal = "Shopping & Personal Expenses"
+    health_wellness = "Health & Wellness"
+    entertainment_leisure = "Entertainment & Leisure"
+    debt_loans = "Debt & Loans"
+
+
 class Gender(Enum):
-    male = "male"
-    female = "female"
+    male = "DFDSFSDF sdfsdf"
+    female = "female sdfsdf"
