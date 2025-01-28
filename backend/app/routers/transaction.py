49857adf852c8faf2c784a85f904fb7ffe7cbc85
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2025-01-28 15:33:06 +0100

    add an endpoint to handle transaction categories obtaining

--- backend/app/routers/transaction.py
+++ backend/app/routers/transaction.py
@@ -8,5 +8,13 @@ from fastapi import APIRouter, Body, Depends, HTTPException, Query, status, Path
 from .. import models, schemas
 from ..database import get_db
 from ..oauth2 import oauth2
+from ..custom_types import TransactionCategory
+
 
 router = APIRouter(prefix="/transactions", tags=["Transactions"])
+
+
+@router.get("/categories")
+async def get_categories():
+
+    return [category.value for category in TransactionCategory]
