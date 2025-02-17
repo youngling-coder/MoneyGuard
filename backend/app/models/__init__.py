3846e5120ef7d67746dec372e105e411e1b8ac07
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2025-02-17 18:49:59 +0100

    add sqlalchemy model to handle expired tokens

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ("base", "User", "Transaction", "Account", "Security_Code_Session")
+__all__ = ("base", "User", "Transaction", "Account", "Security_Code_Session", "ExpiredToken")
 
 from .base import base
 from .user import User
 from .account import Account
 from .transaction import Transaction
 from .security_code_session import Security_Code_Session
+from .expired_token import ExpiredToken
---
+++ backend/app/models/expired_token.py
@@ -0,0 +1,8 @@
+from .base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+
+
+class ExpiredToken(Base):
+    
+    token: Mapped[str] = mapped_column(String, unique=True)
